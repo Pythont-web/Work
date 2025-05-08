94725bbce43dc7353dd021f9240e37e89b60ccbc
Pythont-web <cheremushkin.sancho@mail.ru>
2025-05-08 22:08:45 +0300

    Добавил пошаговость + обновил скринкаст

--- script/backup.php
+++ script/backup.php
@@ -2,25 +2,27 @@
 
 class DatabaseBackup
 {
-    private static $db_name = 'rolldelivery';
-    private static $instance = null;
-    private $connection = null;
-    protected function __construct()
+    private string $db_name;
+    protected \PDO $connection;
+    public function __construct(string $db)
     {
-        $host = '';
-        $user_name = '';
-        $password = '';
-        include "setting.php";
         $this->connection = new \PDO(
-            'mysql:host='.$host.';dbname=' . self::$db_name,
-            $user_name,
-            $password,
+            'mysql:host=localhost;dbname=' . $db,
+            'root',
+            '',
             [
                 \PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION,
                 \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC,
                 \PDO::ATTR_EMULATE_PREPARES => false
             ]
         );
+
+        $this->db_name = $db;
+    }
+
+    public function get_db_name(): string
+    {
+        return $this->db_name;
     }
     protected function __clone()
     {
@@ -31,80 +33,66 @@ class DatabaseBackup
         throw new \BadMethodCallException('Unable to deserialize database connection.');
     }
 
-    public static function getInstance(): DatabaseBackup
+    public function prepare($statement): \PDOStatement
     {
-        if (null == self::$instance)
-        {
-            self::$instance = new self();
-        }
-        return self::$instance;
+        return $this->connection->prepare($statement);
     }
 
-    public static function connection(): \PDO
-    {
-        return self::getInstance()->connection;
-    }
-
-    public static function prepare($statement): \PDOStatement
-    {
-        return static::connection()->prepare($statement);
-    }
-
-    public static function lastInsertId(): int
-    {
-        return intval(static::connection()->lastInsertId());
-    }
 }
 
-class Db_func
+class DatabaseFunctions extends DatabaseBackup
 {
-    private static $db_name = 'rolldelivery';
-
-    public function get_db_name(): string
+    public function __construct(string $db)
     {
-        return self::$db_name;
+        parent::__construct($db);
     }
-    public function get_tables()
+    public function get_tables(): ?array
     {
-        $query = DatabaseBackup::prepare('SHOW TABLES');
+        $query = $this->prepare('SHOW TABLES');
         $query->execute();
-        $tables = $query->fetchALL();
+        $tables = $query->fetchAll();
+
         if (!count($tables))
         {
             return null;
         }
+
         $result = [];
         foreach ($tables as $table)
         {
-            $result[] = $table['Tables_in_' . self::get_db_name()];
+            $result[] = $table['Tables_in_' . $this->get_db_name()];
         }
+
         return $result;
     }
 
-    public function get_table_create($table)
+    public function get_table_create($table) : ?string
     {
-        $query = DatabaseBackup::prepare('SHOW CREATE TABLE ' . $table);
+        $query = $this->prepare('SHOW CREATE TABLE ' . $table);
         $query->execute();
-        $result = $query->fetchALL();
-        if (!count($result))
+        $create = $query->fetchALL();
+
+        if (!count($create))
         {
             return null;
         }
-        return $result[0]['Create Table'];
+
+        return $create[0]['Create Table'];
     }
 
-    public function get_table_value($table)
+    public function get_table_value($table) : ?array
     {
-        $query = DatabaseBackup::prepare('SELECT * FROM ' . $table);
+        $query = $this->prepare('SELECT * FROM ' . $table);
         $query->execute();
-        $result = $query->fetchALL();
-        if (!count($result))
+        $select = $query->fetchALL();
+
+        if (!count($select))
         {
             return null;
         }
-        return $result;
-    }
 
+        return $select;
+    }
 }
 
 function quotes($str): string
@@ -117,74 +105,118 @@ function backticks($str): string
     return ("`". $str. "`");
 }
 
-    $db = new Db_func();
+    $start = microtime(true);
+
+    $db_name = 'wordpress';
+    $db = new DatabaseFunctions($db_name);
 
     $dumpDir = $_SERVER['DOCUMENT_ROOT'].'/dump/';
 
-    if (file_exists($dumpDir))
-    {
-        foreach (glob($dumpDir. '/*') as $file)
-        {
-            unlink($file);
-        }
-    }
-    else
-    {
-        mkdir($dumpDir);
-    }
-
-    $file_name = 'README.sql';
-    $fd = fopen($dumpDir . $file_name, "w");
-
-    fwrite($fd, "-- Для корректного выполнения запросов необходимо выполнить следующие действия:\n
--- 1) Создать базу данных `" . $db->get_db_name() . "` с помощью запроса: \n" . "
-CREATE DATABASE IF NOT EXISTS `" . $db->get_db_name() . "`;\n" . "
--- 2) Перейти к работе с базой данных `" . $db->get_db_name() . "` с помощью запроса: \n" . "
-USE `" . $db->get_db_name() . "`;\n" . "
+    $message = "-- Для корректного выполнения запросов необходимо выполнить следующие действия:\n
+-- 1) Создать базу данных " . backticks($db_name) . " с помощью запроса: \n" . "
+CREATE DATABASE IF NOT EXISTS " . backticks($db_name) . ";\n" . "
+-- 2) Перейти к работе с базой данных " . backticks($db_name) . " с помощью запроса: \n" . "
+USE " . backticks($db_name) . ";\n" . "
 -- 3) Выполнить запросы из всех файлов типа table.sql; \n
--- 4) Выполнить запросы из всех файлов типа table_foreign_keys.sql;");
+-- 4) Выполнить запросы из всех файлов типа table_foreign_keys.sql;";
 
-    $db_name = $db->get_db_name();
+    if(count($_GET) == 0)
+    {
+        if (file_exists($dumpDir))
+        {
+            foreach (glob($dumpDir . '/*') as $file)
+            {
+                unlink($file);
+            }
+        }
+        else
+        {
+            mkdir($dumpDir);
+        }
+
+        $file_name = 'README.sql';
+        $fd = fopen($dumpDir . $file_name, "w");
+
+        fwrite($fd, $message);
+    }
+
     $tables = $db->get_tables();
 
     if(!is_null($tables))
     {
-        foreach ($tables as $table)
+        $table_number = 0;
+
+        if(isset($_GET['table']))
         {
-            $file_name = $table . '.sql';
-            $create = $db->get_table_create($table);
+            $table_number = $_GET['table'];
+        }
+
+        for($i = $table_number; $i < count($tables); $i++)
+        {
+            $file_name = $tables[$i] . '.sql';
+            $create = $db->get_table_create($tables[$i]);
 
             if(!is_null($create))
             {
-                preg_match('/FOREIGN KEY/', $create, $match);
+                preg_match('/FOREIGN KEY[\s\S]*REFERENCES/', $create, $match);
 
                 if(count($match) > 0)
                 {
-                    $file_name = $table . '_foreign_key.sql';
+                    $file_name = $tables[$i] . '_foreign_key.sql';
                 }
 
-                $fd = fopen($dumpDir . $file_name, "w");
-                fwrite($fd, "-- Дамп таблицы `" . $table . "` из базы данных `". $db_name . "`\n\n");
-                fwrite($fd, "-- Создание таблицы: \n\n");
-                fwrite($fd, $create . ";\n\n");
-            }
+                $fd = fopen($dumpDir . $file_name, "a");
 
-            $select = $db->get_table_value($table);
-
-            if(!is_null($select))
-            {
-                fwrite($fd, "-- Заполнение таблицы данными: \n\n");
-                $column = implode(', ', array_map('backticks', array_keys($select[0])));
-
-                for ($j = 0; $j < count($select); $j++)
+                if(!isset($_GET['row']))
                 {
-                    $value = implode(', ', array_map('quotes', $select[$j]));
-                    $insert = "INSERT INTO `" . $table . "` (" . $column. ") VALUES (" . $value . ");";
-
-                    fwrite($fd, $insert . "\n");
+                    fwrite($fd, "-- Дамп таблицы " . backticks($tables[$i]) . " из базы данных ". backticks($db_name) . "\n\n");
+                    fwrite($fd, "-- Создание таблицы: \n\n");
+                    fwrite($fd, $create . ";\n\n");
                 }
+
+                $select = $db->get_table_value($tables[$i]);
+
+                if(!is_null($select))
+                {
+                    if(!isset($_GET['row']))
+                    {
+                        fwrite($fd, "-- Заполнение таблицы данными: \n\n");
+                    }
+
+                    $column = implode(', ', array_map('backticks', array_keys($select[0])));
+
+                    $row_number = 0;
+
+                    if(isset($_GET['row']))
+                    {
+                        $row_number = $_GET['row'];
+                    }
+
+                    for ($j = $row_number; $j < count($select); $j++)
+                    {
+                        $value = implode(', ', array_map('quotes', $select[$j]));
+                        $insert = "INSERT INTO " . backticks($tables[$i]) . " (" . $column. ") VALUES (" . $value . ");";
+
+                        fwrite($fd, $insert . "\n");
+
+                        sleep(1);
+
+                        if(microtime(true) - $start >= 25 && $j < count($select) - 1)
+                        {
+                            header('location: backup.php?table='.$i.'&row='.($j + 1));
+                            exit();
+                        }
+                    }
+
+                }
+
+                fclose($fd);
             }
 
-            fclose($fd);
+            if(isset($_GET['row']) && $i < count($tables) - 1)
+            {
+                header('location: backup.php?table='.($i + 1));
+                exit();
+            }
         }
     }
